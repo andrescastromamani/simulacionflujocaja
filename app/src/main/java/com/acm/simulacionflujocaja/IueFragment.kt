6a24f0c33c530bd3eb568dfa87716c3d33aa3664
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-06-20 16:18:51 -0400

    Validate fields  fragment IUE

--- app/src/main/java/com/acm/simulacionflujocaja/IueFragment.kt
+++ app/src/main/java/com/acm/simulacionflujocaja/IueFragment.kt
@@ -15,7 +15,7 @@ class IueFragment : Fragment(R.layout.fragment_iue) {
     val email=user?.email
     private var _binding: FragmentIueBinding? = null
     private val binding get() = _binding!!
-
+    val r:RedondeoDecimal = RedondeoDecimal()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -28,15 +28,21 @@ class IueFragment : Fragment(R.layout.fragment_iue) {
         val user = FirebaseAuth.getInstance().currentUser
         _binding = FragmentIueBinding.inflate(inflater, container, false)
         val view = binding.root
-        //_binding!!.etDividendo1.setText("100%")
-        recuperarDatos()
-        _binding!!.btnSyncIue.setOnClickListener{
+        //recuperarDatos()
+        /*_binding!!.btnSyncIue.setOnClickListener{
             guardarDatos(user?.email.toString())
-            recuperarDatos()
-        }
+            //recuperarDatos()
+        }*/
+        guardarDatos(user?.email.toString())
         return view
     }
 
+    private fun saveInputsIue(){
+        binding.btnSaveIue.setOnClickListener{
+            validarCampos()
+
+        }
+    }
     private fun guardarDatos(email: String) {
         binding.btnSaveIue.setOnClickListener{
             db.collection("Users").document(user?.email.toString()).collection("Iue").document("DatosIUE").set(
@@ -63,9 +69,44 @@ class IueFragment : Fragment(R.layout.fragment_iue) {
         }
     }
 
+    /*
     private fun recuperarDatos() {
         db.collection("Users").document(email.toString()).collection("Entradas").document("Inputs").get().addOnSuccessListener{
             binding.etAntesImpuestos1.setText(it.get("Ventas contado mes 1") as String?)
+        }
+    }*/
+
+    private fun validarCampos() {
+
+        if (binding.etDividendo1.text.toString().length == 0 ) {
+            binding.etDividendo1.setText("0.0")
+        }
+        if (binding.etPrimas1.text.toString().length == 0) {
+            binding.etPrimas1.setText("0.0")
+        }
+        if (binding.etAntesImpuestos1.text.toString().length == 0) {
+            binding.etAntesImpuestos1.setText("0.0")
+        }
+        if (binding.etGastosDeducibles1.text.toString().length == 0) {
+            binding.etGastosDeducibles1.setText("0.0")
+        }
+        if (binding.etIngresoImponible1.text.toString().length == 0) {
+            binding.etIngresoImponible1.setText("0.0")
+        }
+        if (binding.etUtilidadImpositiva1.text.toString().length == 0) {
+            binding.etUtilidadImpositiva1.setText("0.0")
+        }
+        if (binding.etPorPagar1.text.toString().length == 0) {
+            binding.etPorPagar1.setText("0.0")
+        }
+        if (binding.etDespuesImpuestos1.text.toString().length == 0) {
+            binding.etPrimas1.setText("0.0")
+        }
+        if (binding.etDividendosPagar1.text.toString().length == 0) {
+            binding.etDividendosPagar1.setText("0.0")
+        }
+        if (binding.etPrimasPagar1.text.toString().length == 0) {
+            binding.etPrimasPagar1.setText("0.0")
         }
     }
     companion object {
