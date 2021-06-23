d4fefcb10ccd9d4d2b4bd9baa1e45bab34d60b65
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-06-23 14:20:30 -0400

    Update - Save Data fragment IUE

--- app/src/main/java/com/acm/simulacionflujocaja/IueFragment.kt
+++ app/src/main/java/com/acm/simulacionflujocaja/IueFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import com.acm.simulacionflujocaja.databinding.FragmentIueBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import java.lang.Double.parseDouble
 
 class IueFragment : Fragment(R.layout.fragment_iue) {
     private val db = FirebaseFirestore.getInstance()
@@ -28,53 +29,65 @@ class IueFragment : Fragment(R.layout.fragment_iue) {
         val user = FirebaseAuth.getInstance().currentUser
         _binding = FragmentIueBinding.inflate(inflater, container, false)
         val view = binding.root
-        //recuperarDatos()
-        /*_binding!!.btnSyncIue.setOnClickListener{
-            guardarDatos(user?.email.toString())
-            //recuperarDatos()
-        }*/
-        guardarDatos(user?.email.toString())
+        recuperarDatos()
+        validarCampos()
+        saveInputsIue()
         return view
     }
 
     private fun saveInputsIue(){
         binding.btnSaveIue.setOnClickListener{
             validarCampos()
+            saveTotalsIue()
 
         }
     }
-    private fun guardarDatos(email: String) {
-        binding.btnSaveIue.setOnClickListener{
-            db.collection("Users").document(user?.email.toString()).collection("Iue").document("DatosIUE").set(
-                hashMapOf(
-                    "utilidadAntesImpuestos"    to binding.etAntesImpuestos1.text.toString(),
-                    "gastosDeducibles"          to binding.etGastosDeducibles1.text.toString(),
-                    "ingresosImponibles"        to binding.etIngresoImponible1.text.toString(),
-                    "utilidadImpositiva"        to binding.etUtilidadImpositiva1.text.toString(),
-                    "iuePorPagar"               to binding.etPorPagar1.text.toString(),
-                    "utilidadDespuesImpuestos"  to binding.etDespuesImpuestos1.text.toString(),
-                    "dividendosPorPagar"        to binding.etDividendosPagar1.text.toString(),
-                    "primasPorPagar"            to binding.etPrimasPagar1.text.toString(),
-                ))
-            val utilidadAntesImpuestos:Double = binding.etAntesImpuestos1.text.toString().toDouble()
-            val gastosDeducibles:Double       = binding.etGastosDeducibles1.text.toString().toDouble()
-            val ingresosImpobibles:Double     = binding.etIngresoImponible1.text.toString().toDouble()
-            val utilidadImpositiva:Double     = binding.etUtilidadImpositiva1.text.toString().toDouble()
-            val iuePorPagar:Double     = binding.etPorPagar1.text.toString().toDouble()
-            val utilidadDespuesImpuestos:Double     = binding.etDespuesImpuestos1.text.toString().toDouble()
-            val dividendosPorPagar:Double     = binding.etDividendosPagar1.text.toString().toDouble()
-            val primasPorPagar:Double     = binding.etPrimasPagar1.text.toString().toDouble()
 
-            //val utilidadImpositiva:Double=utilidadAntesImpuestos + gastosDeducibles + ingresosImpobibles
-        }
+    private fun saveTotalsIue() {
+        val iue = 0.25
+        val utilidanAntesImpuestos:Double= parseDouble(binding.etAntesImpuestos1.text.toString())
+        val gastosDeducibles:Double= parseDouble(binding.etGastosDeducibles1.text.toString())
+        val ingresosNoImponibles:Double= parseDouble(binding.etIngresoImponible1.text.toString())
+
+        //Calculo utilidad Ipositiva
+        val utilidadImpositiva:Double=r.redondear(utilidanAntesImpuestos+gastosDeducibles+ingresosNoImponibles)
+        binding.etUtilidadImpositiva1.setText(utilidadImpositiva.toString())
+
+        //Iue por Pagar
+        val iuePorPagar:Double=r.redondear(utilidadImpositiva * iue)
+        binding.etPorPagar1.setText(iuePorPagar.toString())
+
+        //Utilidad despues Impuestos
+        val utilidadDespuesImpuestos:Double=r.redondear(utilidadImpositiva - iuePorPagar)
+        binding.etDespuesImpuestos1.setText(utilidadDespuesImpuestos.toString())
+
+        //GUARDA EN BD TODAS LA ENTRADAS Y SUS CALCULOS
+        db.collection("Users").document(user?.email.toString()).collection("Iue").document("DatosIUE").set(
+            hashMapOf(
+                "utilidadAntesImpuestos"    to binding.etAntesImpuestos1.text.toString(),
+                "gastosDeducibles"          to binding.etGastosDeducibles1.text.toString(),
+                "ingresosImponibles"        to binding.etIngresoImponible1.text.toString(),
+                "utilidadImpositiva"        to binding.etUtilidadImpositiva1.text.toString(),
+                "iuePorPagar"               to binding.etPorPagar1.text.toString(),
+                "utilidadDespuesImpuestos"  to binding.etDespuesImpuestos1.text.toString(),
+                "dividendosPorPagar"        to binding.etDividendosPagar1.text.toString(),
+                "primasPorPagar"            to binding.etPrimasPagar1.text.toString(),
+            )
+        )
     }
 
-    /*
     private fun recuperarDatos() {
-        db.collection("Users").document(email.toString()).collection("Entradas").document("Inputs").get().addOnSuccessListener{
-            binding.etAntesImpuestos1.setText(it.get("Ventas contado mes 1") as String?)
+        db.collection("Users").document(email.toString()).collection("Iue").document("DatosIUE").get().addOnSuccessListener{
+            binding.etAntesImpuestos1.setText(it.get("utilidadAntesImpuestos") as String?)
+            binding.etGastosDeducibles1.setText(it.get("gastosDeducibles") as String?)
+            binding.etIngresoImponible1.setText(it.get("ingresosImponibles") as String?)
+            binding.etUtilidadImpositiva1.setText(it.get("gastosDeducibles") as String?)
+            binding.etPorPagar1.setText(it.get("utilidadImpositiva") as String?)
+            binding.etDespuesImpuestos1.setText(it.get("utilidadDespuesImpuestos") as String?)
+            binding.etDividendosPagar1.setText(it.get("dividendosPorPagar") as String?)
+            binding.etPrimasPagar1.setText(it.get("primasPorPagar") as String?)
         }
-    }*/
+    }
 
     private fun validarCampos() {
 
