f91f59c6ce90f4c2327fe52f623d45cef3a72819
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-06-26 23:42:48 -0400

    Update FlujoFragment

--- app/src/main/java/com/acm/simulacionflujocaja/FlujoFragment.kt
+++ app/src/main/java/com/acm/simulacionflujocaja/FlujoFragment.kt
@@ -13,17 +13,24 @@ import kotlinx.android.synthetic.main.fragment_flujo.*
 import java.lang.Double.parseDouble
 
 class FlujoFragment : Fragment(R.layout.fragment_flujo) {
+
     private val db = FirebaseFirestore.getInstance()
     val user = FirebaseAuth.getInstance().currentUser
     val email=user?.email
     private var _binding: FragmentFlujoBinding? = null
     private val binding get() = _binding!!
     val r:RedondeoDecimal = RedondeoDecimal()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         (activity as MainActivity?)?.getSupportActionBar()?.setTitle("FLUJO EFECTIVO")
+        val user = FirebaseAuth.getInstance().currentUser
         _binding = FragmentFlujoBinding.inflate(inflater, container, false)
         val view = binding.root
         recuperarDatosPresupuesto()
@@ -31,37 +38,10 @@ class FlujoFragment : Fragment(R.layout.fragment_flujo) {
         validarCampos()
         saveInputsFlujo()
         return view
-
-
-    }
-
-    private fun recuperarDatosPresupuesto() {
-        db.collection("Users").document(email.toString()).collection("Entradas").document("OtrosDatos").get().addOnSuccessListener {
-            val totalSalidas:Double= parseDouble(it.get("totalSalidas3") as String?)
-        }
-    }
-
-    private fun recuperarDatos() {
-        db.collection("Users").document(email.toString()).collection("FlujoEfectivoProyectado").document("DatosFlujoEfectivoProyectado").get().addOnSuccessListener{
-            hashMapOf(
-                "flujoEfectivoActividadOperacion"    to binding.etFlujoActividadesOperacion.text.toString(),
-                "ingresosOperacion"    to binding.etIngresosOperacion.text.toString(),
-                "gastosOperacion"    to binding.etGastosOperacion.text.toString(),
-                "flujoActividadesInversion"    to binding.etFlujoEfectivoInversion.text.toString(),
-                "ingresosCapital"    to binding.etIngresosCapital.text.toString(),
-                "gastosCapital"    to binding.etGastosCapital.text.toString(),
-                "flujoActividadesFinanciamiento"    to binding.etFlujoActividadesFinanciamiento.text.toString(),
-                "fuentes"    to binding.etFuentes.text.toString(),
-                "usos"    to binding.etUsos.text.toString(),
-                "incrementoEfectivoPeriodo"    to binding.etIncrementoEfectivoPeriodo.text.toString(),
-                "efectivoInicioPeriodo"    to binding.etEfectivoInicioPeriodo.text.toString(),
-                "saldoEfectivoFinalProyectado"    to binding.etSaldoFinalEfectivoProyectado.text.toString(),
-            )
-        }
     }
 
     private fun saveInputsFlujo(){
-        binding.btnSaveIue.setOnClickListener{
+        binding.btnSaveFlujo.setOnClickListener{
             validarCampos()
             saveTotalsFlujo()
         }
@@ -76,6 +56,8 @@ class FlujoFragment : Fragment(R.layout.fragment_flujo) {
 
         val fuentes:Double= parseDouble(binding.etFuentes.text.toString())
         val usos:Double= parseDouble(binding.etUsos.text.toString())
+
+        val efectivoInicioPeriodo:Double= parseDouble(binding.etEfectivoInicioPeriodo.text.toString())
 
         //Calculo flujo efectivo proyectado por actividades de Operacion
         val flujoProyectadoActividadesOperacion:Double=r.redondear(ingresosOperacion - gastosOperacion)
@@ -98,13 +80,51 @@ class FlujoFragment : Fragment(R.layout.fragment_flujo) {
         binding.etSaldoFinalEfectivoProyectado.setText(saldoFinalProyectado.toString())
 
         //GUARDA EN BD TODAS LA ENTRADAS Y SUS CALCULOS
-        /*db.collection("Users").document(user?.email.toString()).collection("FlujoEfectivoProyecto").document("DatosFlujo").set(
+        db.collection("Users").document(user?.email.toString()).collection("FlujoEfectivoProyectado").document("DatosFlujoProyectado").set(
             hashMapOf(
-                "utilidadAntesImpuestos"    to binding.etAntesImpuestos1.text.toString(),
+                "flujoEfectivoActividadOperacion"    to binding.etFlujoActividadesOperacion.text.toString(),
+                "ingresosOperacion"    to binding.etIngresosOperacion.text.toString(),
+                "gastosOperacion"    to binding.etGastosOperacion.text.toString(),
+                "flujoActividadesInversion"    to binding.etFlujoEfectivoInversion.text.toString(),
+                "ingresosCapital"    to binding.etIngresosCapital.text.toString(),
+                "gastosCapital"    to binding.etGastosCapital.text.toString(),
+                "flujoActividadesFinanciamiento"    to binding.etFlujoActividadesFinanciamiento.text.toString(),
+                "fuentes"    to binding.etFuentes.text.toString(),
+                "usos"    to binding.etUsos.text.toString(),
+                "incrementoEfectivoPeriodo"    to binding.etIncrementoEfectivoPeriodo.text.toString(),
+                "efectivoInicioPeriodo"    to binding.etEfectivoInicioPeriodo.text.toString(),
+                "saldoEfectivoFinalProyectado"    to binding.etSaldoFinalEfectivoProyectado.text.toString(),
             )
-        )*/
+        )
+    }
+    private fun recuperarDatosPresupuesto() {
+        db.collection("Users").document(email.toString()).collection("Entradas").document("DatosPresupuesto").get().addOnSuccessListener {
+            val totalEntradas:Double= parseDouble(it.get("totalEntradas") as String?)
+            val totalSalidas:Double= parseDouble(it.get("totalTotalSalidas") as String?)
+            val saldoAnterior:Double= parseDouble(it.get("saldoAnt1") as String?)
+
+            binding.etIngresosOperacion.setText(totalEntradas.toString())
+            binding.etGastosOperacion.setText(totalSalidas.toString())
+            binding.etEfectivoInicioPeriodo.setText(saldoAnterior.toString())
+        }
     }
 
+    private fun recuperarDatos() {
+        db.collection("Users").document(email.toString()).collection("FlujoEfectivoProyectado").document("DatosFlujoProyectado").get().addOnSuccessListener{
+            binding.etFlujoActividadesOperacion.setText(it.get("flujoEfectivoActividadOperacion") as String?)
+            binding.etIngresosOperacion.setText(it.get("ingresosOperacion") as String?)
+            binding.etGastosOperacion.setText(it.get("gastosOperacion") as String?)
+            binding.etFlujoEfectivoInversion.setText(it.get("flujoActividadesInversion") as String?)
+            binding.etIngresosCapital.setText(it.get("ingresosCapital") as String?)
+            binding.etGastosCapital.setText(it.get("gastosCapital") as String?)
+            binding.etFlujoActividadesFinanciamiento.setText(it.get("flujoActividadesFinanciamiento") as String?)
+            binding.etFuentes.setText(it.get("fuentes") as String?)
+            binding.etUsos.setText(it.get("usos") as String?)
+            binding.etIncrementoEfectivoPeriodo.setText(it.get("incrementoEfectivoPeriodo") as String?)
+            binding.etEfectivoInicioPeriodo.setText(it.get("efectivoInicioPeriodo") as String?)
+            binding.etSaldoFinalEfectivoProyectado.setText(it.get("saldoEfectivoFinalProyectado") as String?)
+        }
+    }
     private fun validarCampos() {
         if (binding.etFlujoActividadesOperacion.text.toString().length == 0 ) {
             binding.etFlujoActividadesOperacion.setText("0.0")
