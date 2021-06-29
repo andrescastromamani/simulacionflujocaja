4abea01362215816cf6a074d41c76fe3c6ddb6ad
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-06-29 11:37:35 -0400

    Update IUE Design

--- app/src/main/java/com/acm/simulacionflujocaja/IueFragment.kt
+++ app/src/main/java/com/acm/simulacionflujocaja/IueFragment.kt
@@ -48,52 +48,59 @@ class IueFragment : Fragment(R.layout.fragment_iue) {
         val destinadoDividendos:Double= parseDouble(binding.etDestinadoDividendos.text.toString())
         val destinadoPagoPrima:Double= parseDouble(binding.etDestinadoPagosPrimas.text.toString())
 
-        val utilidanAntesImpuestos:Double= parseDouble(binding.etAntesImpuestos1.text.toString())
-        val gastosDeducibles:Double= parseDouble(binding.etGastosDeducibles1.text.toString())
-        val ingresosNoImponibles:Double= parseDouble(binding.etIngresoImponible1.text.toString())
+        val utilidanAntesImpuestos:Double= parseDouble(binding.etUtilidadAntesImpuestos.text.toString())
+        val gastosDeducibles:Double= parseDouble(binding.etGastosDeducibles.text.toString())
+        val ingresosNoImponibles:Double= parseDouble(binding.etIngresosNoImponibles.text.toString())
 
         //Calculo utilidad Ipositiva
         val utilidadImpositiva:Double=r.redondear(utilidanAntesImpuestos + gastosDeducibles - ingresosNoImponibles)
-        binding.etUtilidadImpositiva1.setText(utilidadImpositiva.toString())
+        binding.etUtilidadImpositiva.setText(utilidadImpositiva.toString())
 
         //Iue por Pagar
         val iuePorPagar:Double=r.redondear(utilidadImpositiva * iue)
-        binding.etPorPagar1.setText(iuePorPagar.toString())
+        binding.etIuePorPagar.setText(iuePorPagar.toString())
 
         //Utilidad despues Impuestos
         val utilidadDespuesImpuestos:Double=r.redondear(utilidadImpositiva - iuePorPagar)
-        binding.etDespuesImpuestos1.setText(utilidadDespuesImpuestos.toString())
+        binding.etUtilidadDespuesImpuestos.setText(utilidadDespuesImpuestos.toString())
 
         //Dividendos Por Pagar
         val dividendosPorPagar:Double=r.redondear(utilidadDespuesImpuestos*(destinadoDividendos/100))
-        binding.etDividendosPagar1.setText(dividendosPorPagar.toString())
+        binding.etDividendosPorPagar.setText(dividendosPorPagar.toString())
 
         //Dividendos Por Pagar
         val primasPorPagar:Double=r.redondear(utilidadDespuesImpuestos*(destinadoPagoPrima/100))
-        binding.etPrimasPagar1.setText(primasPorPagar.toString())
+        binding.etPrimasPorPagar.setText(primasPorPagar.toString())
 
         //GUARDA EN BD TODAS LA ENTRADAS Y SUS CALCULOS
         db.collection("Users").document(user?.email.toString()).collection("Iue").document("DatosIUE").set(
             hashMapOf(
-                "utilidadAntesImpuestos"    to binding.etAntesImpuestos1.text.toString(),
-                "gastosDeducibles"          to binding.etGastosDeducibles1.text.toString(),
-                "ingresosImponibles"        to binding.etIngresoImponible1.text.toString(),
-                "utilidadImpositiva"        to binding.etUtilidadImpositiva1.text.toString(),
-                "iuePorPagar"               to binding.etPorPagar1.text.toString(),
-                "utilidadDespuesImpuestos"  to binding.etDespuesImpuestos1.text.toString(),
-                "dividendosPorPagar"        to binding.etDividendosPagar1.text.toString(),
-                "primasPorPagar"            to binding.etPrimasPagar1.text.toString(),
+                "destinadoDividendos"    to binding.etDestinadoDividendos.text.toString(),
+                "destinadoPagosPrimas"    to binding.etDestinadoPagosPrimas.text.toString(),
+                "utilidadAntesImpuestos"    to binding.etUtilidadAntesImpuestos.text.toString(),
+                "gastosDeducibles"          to binding.etGastosDeducibles.text.toString(),
+                "ingresosNoImponibles"        to binding.etIngresosNoImponibles.text.toString(),
+                "utilidadImpositiva"        to binding.etUtilidadImpositiva.text.toString(),
+                "iuePorPagar"               to binding.etIuePorPagar.text.toString(),
+                "utilidadDespuesImpuestos"  to binding.etUtilidadDespuesImpuestos.text.toString(),
+                "dividendosPorPagar"        to binding.etDividendosPorPagar.text.toString(),
+                "primasPorPagar"            to binding.etPrimasPorPagar.text.toString(),
             )
         )
     }
 
     private fun recuperarDatos() {
         db.collection("Users").document(email.toString()).collection("Iue").document("DatosIUE").get().addOnSuccessListener{
-            binding.etAntesImpuestos1.setText(it.get("utilidadAntesImpuestos") as String?)
-            binding.etGastosDeducibles1.setText(it.get("gastosDeducibles") as String?)
-            binding.etIngresoImponible1.setText(it.get("ingresosImponibles") as String?)
-            binding.etPorPagar1.setText(it.get("utilidadImpositiva") as String?)
-            binding.etDespuesImpuestos1.setText(it.get("utilidadDespuesImpuestos") as String?)
+            binding.etDestinadoDividendos.setText(it.get("destinadoDividendos") as String?)
+            binding.etDestinadoPagosPrimas.setText(it.get("destinadoPagosPrimas") as String?)
+            binding.etUtilidadAntesImpuestos.setText(it.get("utilidadAntesImpuestos") as String?)
+            binding.etGastosDeducibles.setText(it.get("gastosDeducibles") as String?)
+            binding.etIngresosNoImponibles.setText(it.get("ingresosNoImponibles") as String?)
+            binding.etUtilidadImpositiva.setText(it.get("utilidadImpositiva") as String?)
+            binding.etIuePorPagar.setText(it.get("iuePorPagar") as String?)
+            binding.etUtilidadDespuesImpuestos.setText(it.get("utilidadDespuesImpuestos") as String?)
+            binding.etDividendosPorPagar.setText(it.get("dividendosPorPagar") as String?)
+            binding.etPrimasPorPagar.setText(it.get("primasPorPagar") as String?)
         }
     }
 
@@ -105,29 +112,29 @@ class IueFragment : Fragment(R.layout.fragment_iue) {
         if (binding.etDestinadoPagosPrimas.text.toString().length == 0) {
             binding.etDestinadoPagosPrimas.setText("0.0")
         }
-        if (binding.etAntesImpuestos1.text.toString().length == 0) {
-            binding.etAntesImpuestos1.setText("0.0")
+        if (binding.etUtilidadAntesImpuestos.text.toString().length == 0) {
+            binding.etUtilidadAntesImpuestos.setText("0.0")
         }
-        if (binding.etGastosDeducibles1.text.toString().length == 0) {
-            binding.etGastosDeducibles1.setText("0.0")
+        if (binding.etGastosDeducibles.text.toString().length == 0) {
+            binding.etGastosDeducibles.setText("0.0")
         }
-        if (binding.etIngresoImponible1.text.toString().length == 0) {
-            binding.etIngresoImponible1.setText("0.0")
+        if (binding.etIngresosNoImponibles.text.toString().length == 0) {
+            binding.etIngresosNoImponibles.setText("0.0")
         }
-        if (binding.etUtilidadImpositiva1.text.toString().length == 0) {
-            binding.etUtilidadImpositiva1.setText("0.0")
+        if (binding.etUtilidadImpositiva.text.toString().length == 0) {
+            binding.etUtilidadImpositiva.setText("0.0")
         }
-        if (binding.etPorPagar1.text.toString().length == 0) {
-            binding.etPorPagar1.setText("0.0")
+        if (binding.etIuePorPagar.text.toString().length == 0) {
+            binding.etIuePorPagar.setText("0.0")
         }
-        if (binding.etDespuesImpuestos1.text.toString().length == 0) {
-            binding.etDespuesImpuestos1.setText("0.0")
+        if (binding.etUtilidadDespuesImpuestos.text.toString().length == 0) {
+            binding.etUtilidadDespuesImpuestos.setText("0.0")
         }
-        if (binding.etDividendosPagar1.text.toString().length == 0) {
-            binding.etDividendosPagar1.setText("0.0")
+        if (binding.etDividendosPorPagar.text.toString().length == 0) {
+            binding.etDividendosPorPagar.setText("0.0")
         }
-        if (binding.etPrimasPagar1.text.toString().length == 0) {
-            binding.etPrimasPagar1.setText("0.0")
+        if (binding.etPrimasPorPagar.text.toString().length == 0) {
+            binding.etPrimasPorPagar.setText("0.0")
         }
     }
     companion object {
