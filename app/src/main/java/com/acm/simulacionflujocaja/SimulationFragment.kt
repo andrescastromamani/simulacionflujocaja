0927cd05b11f48852b0eaed3903efb7325140223
Ricardo <ric75711904@gmail.com>
2021-07-04 12:57:46 -0400

    213

--- app/src/main/java/com/acm/simulacionflujocaja/SimulationFragment.kt
+++ app/src/main/java/com/acm/simulacionflujocaja/SimulationFragment.kt
@@ -1,44 +1,36 @@
 package com.acm.simulacionflujocaja
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.acm.simulacionflujocaja.databinding.FragmentFlujoBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.acm.simulacionflujocaja.databinding.FragmentSimulationBinding
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SimulationFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
+
 class SimulationFragment : Fragment(R.layout.fragment_simulation) {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentSimulationBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_simulation, container, false)
+        (activity as MainActivity?)?.getSupportActionBar()?.setTitle("Simulacion")
+        binding.btnTri.setOnClickListener {
+
+        }
+        return view
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance() = IueFragment()
-    }
+
 }
